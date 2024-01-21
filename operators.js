16f8c7b6fd47025afd10e8124b7355159e015cab
Rithika C <rithikac.21ece@kongu.edu>
2024-01-21 20:58:48 +0530

    JS

---
+++ operators.js
@@ -0,0 +1,33 @@
+//Assignment operator
+let a=10
+console.log("Assignment operator Output: ",a)
+//Arithmetic operator -> +,-,*,/,%
+var aA = 10
+var bA = 10
+console.log(typeof aA)
+console.log("Arithmetic operator addition output: ",aA+bA)
+console.log("Arithmetic operator subraction output: ",aA-bA)
+console.log("Arithmetic operator division output: ",aA/bA)
+console.log("Arithmetic operator multiplication output: ",aA*bA)
+console.log("Arithmetic operator modulus output: ",aA%bA)
+//Logical operator -> &&,||,!
+var aA=0
+var bA=20
+console.log("Logical operator && output: ",aA&&bA)
+console.log("Logical operator || output: ",aA||bA)
+console.log("Logical operator && output: ",!bA)
+console.log(10 && 20)
+console.log(30 && 20)
+console.log(0 && 20)
+console.log(10 && 0)
+//!10 --> false
+//!0 -->true
+//!-10 -->false
+console.log(10 || 20)
+console.log(0 || 20)
+console.log(100 || 20 && 0)
+
+
+
+
+
---
+++ variable.js
@@ -0,0 +1,10 @@
+a='a'
+console.log("General variable declaration a='a': ",a)
+var b='var'
+console.log("This variable belongs to var type var b='var':",b)
+let c='let'
+console.log('This variable belongs to let, let c="let" type:',c)
+const d=100
+console.log("Constant variable const d=100 value is: ",d)
+d = 200
+console.log(d)
